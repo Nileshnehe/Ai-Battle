e3571987cadccd524e48467e7e2bb55abe7922e4
Nileshnhe <nehenilesh111@gmail.com>
2026-05-04 17:00:34 +0530

    chore: add .gitignore and configure server to listen on specified port

--- server/server.ts
+++ server/server.ts
@@ -2,4 +2,8 @@ import app from "./src/app.js"
 import dotenv from "dotenv"
 
 dotenv.config()
-app.listen()
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
